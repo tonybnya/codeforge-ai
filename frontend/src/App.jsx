011ce554853f629e256f70cc16dacf81b90a5a1f
tonybnya <nya.tony2010@gmail.com>
2025-06-02 22:14:00 +0100

    feat: create some components and templates + add routing between components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import { ClerkProviderWithRoutes } from "./auth/ClerkProviderWithRoutes.jsx";
+import ClerkProviderWithRoutes from "./auth/ClerkProviderWithRoutes.jsx";
+import ChallengesHistory from "./pages/ChallengesHistory.jsx";
+import Layout from "./pages/Layout.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import AuthenticationPage from "./auth/AuthenticationPage.jsx";
 import "./App.css";
 
 const App = () => {
   return (
-    <ClerkProviderWithRoutes>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </ClerkProviderWithRoutes>
+    <>
+      <Routes>
+        <Route path="signin/*" element={<AuthenticationPage />} />
+        <Route path="signup" element={<AuthenticationPage />} />
+        <Route element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="home" element={<LandingPage />} />
+          <Route path="challenges-history" element={<ChallengesHistory />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ frontend/src/pages/ChallengesHistory.jsx
@@ -0,0 +1,9 @@
+const ChallengesHistory = () => {
+  return (
+    <div>
+      <h1>ChallengesHistory</h1>
+    </div>
+  );
+};
+
+export default ChallengesHistory;
